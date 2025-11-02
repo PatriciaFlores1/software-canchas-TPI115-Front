0c5e0cd7bc4c8da929b6dbf6630b7f698c71a15c
0JD0 <eusojdivad0@gmail.com>
2025-11-02 12:27:54 -0600

    feat: Add initial admin user to the usuarios table

--- sistema_reserva.sql
+++ sistema_reserva.sql
@@ -227,3 +227,9 @@ INSERT INTO `roles` (`nombre`) VALUES
   ('admin'),
   ('cliente'),
   ('propietario');
+
+-- --------------------------------------------------------
+-- INSERTS: usuarios (Solo admin)
+-- --------------------------------------------------------
+INSERT INTO `usuarios` (`nombre`, `apellido`, `email`, `password`, `telefono`, `id_rol`, `fecha_registro`, `id_estado`, `url_foto`) VALUES
+('Admin', 'canchas', 'admincanchas@gmail.com', '$2y$10$EjvrU/9loLcaEhZ3hlaAnO6SadB0MLvrFlvoiXPWsXDThXKqRbfo.', '75906148', 1, '2025-11-01 09:39:31', 1, NULL),
